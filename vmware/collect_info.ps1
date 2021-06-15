a6ab447f599d6671e253d54bce6303ab5e53c231
pomelo <pomelo@outlook.com>
2021-06-15 16:58:10 +0800

    update vmware.ps1

--- vmware/collect_info.ps1
+++ vmware/collect_info.ps1
@@ -18,12 +18,25 @@ $collect_info=""
 $vmware_hostinfo = (Get-Vmhost | select).Name
 # 查询vm宿主机对应的版本号
 $vmware_versioninfo = (Get-Vmhost | select).Version
+# 查询vm宿主机cpu个数
+$vmware_numcpu = (Get-Vmhost | select).NumCpu
+# 查询vm宿主机cpu
+$vmware_cputotal = (Get-Vmhost | select).CpuTotalMhz
+# 查询vm宿主机总内存
+$vmware_memtotal = (Get-Vmhost | select).MemoryTotalGB
+# 查询vm集群信息
+$vmware_cluster = $(Get-Cluster |select).Name
+
 
 # for循环进行格式化数据
 # $vmware_hostinfo.count：统计数据行数，类似linux的wc -l命令
-for ($i=1;$i -lt $vmare_hostinfo.count; $i++) {
+for ($i=1;$i -lt $vmare_hostinfo.Count; $i++) {
+    # 获取宿主机对应的集群
+    $vmware_parent = (Get-Vmhost -name $vmware_hostinfo[$i] | select -ExpandProperty "Parent" | select ).Name
+    # 格式化浮点数
+    $mem_int = [math]::round($vmware_memtotal[$i],0)
     # 根据下标去对应的数据值
-    $collect_info += "name:" + $vmare_hostinfo[$i] + ",version:" + $vmware_versioninfo[$i] 
+    $collect_info += "name:" + $vmare_hostinfo[$i] + ",version:" + $vmware_versioninfo[$i] + ",numcpu:" + $vmware_numcpu[$i] + ",totalcpu:" + $vmware_cputotal[$i] + ",totalmem:" + $vmware_memtotal[$i]
 }
 
 # 输出采集后的数据
